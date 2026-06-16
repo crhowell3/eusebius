b61aec665f3973af92129619b12832cdc0a1e81a
Cameron Howell <jango.djarin@proton.me>
2026-06-15 23:08:27 -0500

    Some CSS tweaks

--- src/tabs/settings.rs
+++ src/tabs/settings.rs
@@ -3,11 +3,8 @@ use yew::prelude::*;
 #[function_component(Settings)]
 pub fn settings() -> Html {
     html! {
-        <div class="menu-backdrop">
-            <div class="menu-panel">
-                <h2>{"Settings"}</h2>
-                <p>{"Placeholder"}</p>
-            </div>
+        <div>
+            {"Placeholder"}
         </div>
     }
 }
